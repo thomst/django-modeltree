6c6f7eb59afd4aa15e5f49f6e3404406d21704a6
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-03-05 14:59:59 +0100

    [tests] create test data for ModelOne serie

--- tests/testapp/management/commands/createtestdata.py
+++ tests/testapp/management/commands/createtestdata.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from testapp.models import ModelA, ModelB, ModelC, ModelD, ModelE
+from testapp.models import ModelOne, ModelTwo, ModelThree, ModelFour, ModelFive
 
 
 def create_test_data():
+    # create admin-user
+    User.objects.all().delete()
+    User.objects.create_superuser('admin', password='adminpassword')
+
     # clear existing data
     ModelA.objects.all().delete()
     ModelB.objects.all().delete()
@@ -61,6 +67,50 @@ def create_test_data():
         if i in range(6, 12):
             objs = list(ModelB.objects.filter(pk__in=range(26, 30)))
             model_e.model_b.add(*objs)
+
+
+    # Create test-data for ModelOne serie.
+    ModelOne.objects.all().delete()
+    ModelTwo.objects.all().delete()
+    ModelThree.objects.all().delete()
+    ModelFour.objects.all().delete()
+    ModelFive.objects.all().delete()
+    [ModelOne(id=i).save() for i in range(4)]
+    [ModelTwo(id=i).save() for i in range(8)]
+    [ModelThree(id=i).save() for i in range(8)]
+    [ModelFour(id=i).save() for i in range(8)]
+    [ModelFive(id=i).save() for i in range(8)]
+
+    obj_one = ModelOne.objects.get(id=3)
+    obj_one.model_two.set(ModelTwo.objects.filter(id__in=range(3)))
+    obj_one = ModelOne.objects.get(id=2)
+    obj_one.model_two.set(ModelTwo.objects.filter(id__in=range(3,5)))
+
+    for obj in ModelOne.objects.all():
+        obj.user = User.objects.all()[0]
+        obj.save()
+
+    obj_two = ModelTwo.objects.get(id=7)
+    obj_two.model_one.set(ModelOne.objects.filter(id__in=range(4)))
+
+    objs_two = ModelTwo.objects.filter(id__in=range(4,8))
+    for index, obj in enumerate(objs_two):
+        obj.model_three = ModelThree.objects.get(id=index%2)
+        obj.save()
+
+    for obj in ModelThree.objects.filter(id__in=range(2)):
+        obj.model_five.set(ModelFive.objects.filter(id__in=range(3, 6)))
+
+    for obj in ModelFour.objects.all():
+        obj.model_three = ModelThree.objects.get(pk=obj.id)
+        obj.save()
+
+    # from modeltree import ModelTree
+    # class MyModelTree(ModelTree):
+    #     FOLLOW_ACROSS_APPS = True
+    # items = ModelOne.objects.filter(id__in=range(4))
+    # tree = MyModelTree(ModelOne, items)
+    # tree.show('[{node.relation_type}] {node.field.name}', with_items=True)
 
 
 class Command(BaseCommand):
